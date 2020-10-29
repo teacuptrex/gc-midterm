b7bfb01c54c4a7849d1459d3978f603b2422d9f6
Hunter Szakacs <hunterszakacs@gmail.com>
2020-10-29 10:01:12 -0400

    initial

--- src/MidTerm.java
+++ src/MidTerm.java
@@ -5,7 +5,7 @@ public class MidTerm {
 		// TODO Auto-generated method stub
 
 		System.out.println("Once upon a time in the mystical land of Shangrila there dwelt an immortal named Kai-Yun and his trusty band of ninja monkeys.\r\n");
-		
+		System.out.println("Kai-Yun had a wife named Meili.");
 	}
 
 }
