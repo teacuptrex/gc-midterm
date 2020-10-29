b142b64ac0b528f0e1d591cc5ffab20329ecf837
Logan <lmlopus@gmail.com>
2020-10-29 10:11:07 -0400

    Fifth commit. Logan's Second

--- src/MidTerm.java
+++ src/MidTerm.java
@@ -7,6 +7,7 @@ public class MidTerm {
 		System.out.println("Once upon a time in the mystical land of Shangrila there dwelt an immortal named Kai-Yun and his trusty band of ninja monkeys.\r\n");
 		System.out.println("As is well known, ninja monkeys all gain their extraordinary ninja powers following Sun Wukong, the Monkey King god.");
 		System.out.println("Kai-Yun had a wife named Meili.");
+		System.out.println("One day Kai-Yun and Meili were out in the park on a walk. Suddenly Meili turns to Kai-Yun and goes I have not been forthright with you. I am not who I appear to be");
 
 	}
 
