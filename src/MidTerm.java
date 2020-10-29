32370d9870cc9d383d59e2d454cc99e14a944795
Hunter Szakacs <hunterszakacs@gmail.com>
2020-10-29 10:18:53 -0400

    second line

--- src/MidTerm.java
+++ src/MidTerm.java
@@ -8,7 +8,7 @@ public class MidTerm {
 		System.out.println("As is well known, ninja monkeys all gain their extraordinary ninja powers following Sun Wukong, the Monkey King god.");
 		System.out.println("Kai-Yun had a wife named Meili.");
 		System.out.println("One day Kai-Yun and Meili were out in the park on a walk. Suddenly Meili turns to Kai-Yun and goes I have not been forthright with you. I am not who I appear to be");
-
+		System.out.println("All of a sudden, Meili starts to glow a bright gold color. She slowly starts to peel the skin off her face, reveling a hideous monkey like face. A once beautiful woman Meili has now revealed herself to be Sun Wukong, the Monkey King god.");       "
 	}
 
 }
