6e6aaa7971778f6d03decb41aa158c482fff0bf2
Logan <lmlopus@gmail.com>
2020-10-29 14:38:34 -0400

    First POS commit

--- src/MidTerm.java
+++ src/MidTerm.java
@@ -1,26 +1,51 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class MidTerm {
+	static Scanner scnr = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
-<<<<<<< HEAD
-		System.out.println("Once upon a time in the mystical land of Detroit there dwelt an immortal named David and his trusty band of ninja students.");
-		System.out.println("As is well known, ninja students all gain their extraordinary ninja powers following Sun Wukong, the Monkey King god.");
-		System.out.println("David had a wife named Meili.");
-		System.out.println("One day David and Meili were out in the park on a walk. Suddenly Meili turns to David and goes I have not been forthright with you. I am not who I appear to be");
-		System.out.println("All of a sudden, Meili starts to glow a bright gold color. She slowly starts to peel the skin off her face, reveling a hideous monkey like face. A once beautiful woman Meili has now revealed herself to be Sun Wukong, the Monkey King god.");       
-		System.out.println("Shocked by the grotesque transformation, David jaw dropped as he fell to his knees.  \"Does this mean... ...our children?\" David asked hesitantly, afraid to know the answer.");
-=======
+		boolean flag = true;
+		do {
+			System.out.println("Welcome to BurgerFi! Please take a look at our menu and place an order.");
+			// displayMenu();
+			// int order = scnr.nextInt();
+			System.out.println("Would you like to place another order?");
+		} while (flag);
+		System.out.println("How will you be paying for that? Cash, credit, or check?");
+		// printReciept();
+		// String paymentMethod = scnr.next();
+		// getChange()
+		if (Validator.getYesNo(scnr, "Would you like to keep adding to the order? yes or no")) {
 
-		System.out.println("Once upon a time in the mystical land of Shangrila there dwelt an immortal named David and his trusty band of ninja students.");
-		System.out.println("As is well known, ninja students all gain their extraordinary ninja powers following Sun Wukong, the Monkey King god.");
-		System.out.println("David had a wife named Meili.");
-		System.out.println("One day Davidgit pull and Meili were out in the park on a walk. Suddenly Meili turns to Kai-Yun and goes I have not been forthright with you. I am not who I appear to be");
-		System.out.println("All of a sudden, Meili starts to glow a bright gold color. She slowly starts to peel the skin off her face, reveling a hideous student-like face. A once beautiful woman Meili has now revealed herself to be Sun Wukong, the Monkey King god.");       
-		System.out.println("Shocked by the grotesque transformation, David jaw dropped as he fell to his knees.  \"Does this mean... ...our children?\" Kai-Yun asked hesitantly, afraid to know the answer.");
->>>>>>> d98c4e9134bae21f394296f9701af6145f187ab3
+		} else {
+			flag = false;
+		}
+	}
+
+	public static ArrayList<Meal> createMeals() {
+		
+		return null;
+
+	}
+	
+	public static void displayMenu(ArrayList<Meal> meals) {
 
 	}
 
+	public static void takeOrder() {
+		
+	}
+	
+	public static void printReceipt() {
+		
+	}
+	
+	public static void getChange() {
+		
+	}
+	
+	
+	
 }
