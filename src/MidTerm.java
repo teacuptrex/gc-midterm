a415903142040d4033b354fd61dc7e17d68810e9
jayme <jaymeipema@gmail.com>
2020-10-29 10:06:53 -0400

    ji 1

--- src/MidTerm.java
+++ src/MidTerm.java
@@ -5,7 +5,9 @@ public class MidTerm {
 		// TODO Auto-generated method stub
 
 		System.out.println("Once upon a time in the mystical land of Shangrila there dwelt an immortal named Kai-Yun and his trusty band of ninja monkeys.\r\n");
+		System.out.println("As is well known, ninja monkeys all gain their extraordinary ninja powers following Sun Wukong, the Monkey King god.");
 		System.out.println("Kai-Yun had a wife named Meili.");
+
 	}
 
 }
